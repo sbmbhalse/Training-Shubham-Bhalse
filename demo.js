24c0ddf470f68ba1f334ae42475dc6a0d2782f01
sbmbhalse <shubhambhalse8@gmail.com>
2018-07-05 20:40:48 +0530

    added html files

---
+++ demo.js
@@ -0,0 +1,5 @@
+
+function myFunction(){
+document.getElementById("d").innerHTML="A simple paragraph has  changed also for coloring purpose i have applied the external and java script both but in this paragraph i can not change  its styling but let see if it could be changed by external css ";
+
+}
